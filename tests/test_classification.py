e91910ffdfe4206eb020c3cbc6f97eb2ef3579f7
Nishant Yadav <nishantyadav@microsoft.com>
2023-05-23 14:07:41 -0700

    inherit threshold accuracy from torchmetrics

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -6,44 +6,21 @@ from visionmetrics.classification import ThresholdAccuracy
 
 class TestClassification(unittest.TestCase):
 
-    TARGETS = [
-        torch.tensor([1, 0, 0, 0, 1, 1, 0, 0, 0, 1]),
-        torch.tensor([1, 0, 2, 0, 1, 2, 0, 0, 0, 1, 2, 1, 2, 2, 0])]
-    PREDICTIONS = [
-        torch.tensor([[1, 0],
-                      [0, 1],
-                      [0.5, 0.5],
-                      [0.1, 0.9],
-                      [0.44, 0.56],
-                      [0.09, 0.91],
-                      [0.91, 0.09],
-                      [0.37, 0.63],
-                      [0.34, 0.66],
-                      [0.89, 0.11]]),
-        torch.tensor([[0.99, 0.01, 0],
-                      [0, 0.99, 0.01],
-                      [0.51, 0.49, 0.0],
-                      [0.09, 0.8, 0.11],
-                      [0.34, 0.36, 0.3],
-                      [0.09, 0.90, 0.01],
-                      [0.91, 0.06, 0.03],
-                      [0.37, 0.60, 0.03],
-                      [0.34, 0.46, 0.2],
-                      [0.79, 0.11, 0.1],
-                      [0.34, 0.16, 0.5],
-                      [0.04, 0.56, 0.4],
-                      [0.04, 0.36, 0.6],
-                      [0.04, 0.36, 0.6],
-                      [0.99, 0.01, 0.0]])]
+    TARGETS = [torch.tensor([[0, 1, 0], [1, 0, 1]]),
+               torch.tensor([[1, 1, 0], [1, 0, 1]])]
+
+    PREDICTIONS = [torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.8, 0.6]]),
+                   torch.tensor([[0.2, 0.6, 0.7], [0.1, 0.3, 0.7]])]
 
     def test_threshold_accuracy(self):
-        gts = [[0.4, 0.35, 0.2], [0.355555, 0.4, 0.133333]]
+        gts = [[0.333, 0.333, 0.333], [0.5, 0.5, 0.333]]
+
         for i, (targets, predictions) in enumerate(zip(self.TARGETS, self.PREDICTIONS)):
             for j, threshold in enumerate(['0.3', '0.5', '0.7']):
-                metric = ThresholdAccuracy(float(threshold))
+                metric = ThresholdAccuracy(num_labels=3, threshold=float(threshold))
                 metric.update(predictions, targets)
                 metric_output = metric.compute()[f"accuracy_thres={threshold}"].item()
-                self.assertAlmostEqual(metric_output, gts[i][j], places=5)
+                self.assertAlmostEqual(metric_output, gts[i][j], places=3)
 
 
 if __name__ == '__main__':
--- visionmetrics/classification/threshold_accuracy.py
+++ visionmetrics/classification/threshold_accuracy.py
@@ -1,75 +1,23 @@
-import torch
-from torchmetrics import Metric
-
-from visionmetrics import utils
+from torch import Tensor
+from torchmetrics.classification import MultilabelAccuracy
 
 
-class ThresholdAccuracy(Metric):
+class ThresholdAccuracy(MultilabelAccuracy):
     """
-    Threshold-based accuracy evaluator for multilabel classification, calculated in a sample-based flavor
-    Note that
-        1. this could be used for multi-class classification, but does not make much sense
-        2. sklearn.metrics.accuracy_score actually is computing exact match ratio for multi-label classification, which is too harsh
+    Calculates the accuracy for multi-label classification tasks given a threshold.
+    Note: this could be used for multi-class classification, but does not make much sense
     """
 
-    def __init__(self, threshold) -> None:
-        super().__init__()
-        self.prediction_filter = ThresholdPredictionFilter(threshold)
+    def _identifier(self):
+        return f'thres={self.threshold}'
 
-        self.add_state("num_samples", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("sample_accuracy_sum", default=torch.tensor(0.0), dist_reduce_fx="sum")
-
-    def update(self, predictions: torch.Tensor, targets: torch.Tensor) -> None:
+    def update(self, predictions: Tensor, targets: Tensor) -> None:
         """ Add a batch of predictions for evaluation.
         Args:
             predictions (torch.tensor): the model output array. Shape (N, num_class)
-            targets: the ground truths (torch.tensor). Shape (N, num_class) for multi-label (or (N,) for multi-class)
+            targets (torch.tensor): the ground truths. Shape (N, num_class) for multi-label or (N,1) for multi-class
         """
-
-        assert len(predictions) == len(targets)
-
-        num_samples = len(predictions)
-        target_mat = utils.targets_to_mat(targets, predictions.shape[1])
-
-        prediction_over_threshold = self.prediction_filter.filter(predictions, 'vec')
-        n_correct_predictions = torch.mul(prediction_over_threshold, target_mat).sum(1)  # shape (N,)
-        n_total = (torch.add(prediction_over_threshold, target_mat) >= 1).sum(1)  # shape (N,)
-        n_total[n_total == 0] = 1  # To avoid zero-division. If n_total==0, num should be zero as well.
-
-        self.sample_accuracy_sum += (n_correct_predictions / n_total).sum()
-        self.num_samples += num_samples
+        return super().update(predictions, targets)
 
     def compute(self) -> dict:
-        return {f'accuracy_{self.prediction_filter.identifier}': float(self.sample_accuracy_sum) / self.num_samples if self.num_samples else 0.0}
-
-
-class ThresholdPredictionFilter:
-    def __init__(self, threshold: float):
-        """
-        Args:
-            threshold: confidence threshold
-        """
-
-        self.threshold = threshold
-
-    def filter(self, predictions, return_mode):
-        """ Return predictions over confidence over threshold
-        Args:
-            predictions (torch.tensor): the model output array. Shape (N, num_class)
-            return_mode: can be 'indices' or 'vec', indicating whether return value is a set of class indices or 0-1 vector
-
-        Returns:
-            (list): labels with probabilities over threshold, for each sample
-        """
-        if return_mode == 'indices':
-            preds_over_thres = [[] for _ in range(len(predictions))]
-            for indices in torch.argwhere(predictions >= self.threshold):
-                preds_over_thres[indices[0]].append(indices[1])
-
-            return preds_over_thres
-        else:
-            return predictions >= self.threshold
-
-    @property
-    def identifier(self):
-        return f'thres={self.threshold}'
+        return {f'accuracy_{self._identifier()}': super().compute()}
