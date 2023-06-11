13ab64d386ede707723436c425fc38e0e006a318
Nishant Yadav <nishantyadav@microsoft.com>
2023-06-11 12:02:48 -0700

    add all classification tests

--- visionmetrics/classification/__init__.py
+++ visionmetrics/classification/__init__.py
@@ -1,5 +1,8 @@
 # Import metrics directly from torchmetrics
-from torchmetrics.classification import Accuracy
+from torchmetrics.classification import (Accuracy, AveragePrecision,
+                                         CalibrationError, MulticlassAccuracy,
+                                         MulticlassPrecision,
+                                         MultilabelAccuracy)
 
 # Import custom metrics from visionmetrics
 from visionmetrics.classification.precision_recall import (MultilabelF1Score,
