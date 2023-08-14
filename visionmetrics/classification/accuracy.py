39a01e057cb3591c2bb7a9faa91d4266e03a6aa1
Nishant Yadav <108902158+nisyad-ms@users.noreply.github.com>
2023-08-14 12:40:20 -0700

    Nisyad/modify multiclass topk (#39)
    * Add MulticlassMixin
    - allows multiclass metrics to accept topk > num_classes
    
    * add test case for topk > num_classes
    
    * minor fix
    
    * fix flake8

---
+++ visionmetrics/classification/accuracy.py
@@ -0,0 +1,6 @@
+import torchmetrics
+from visionmetrics.classification.common import MulticlassMixin
+
+
+class MulticlassAccuracy(MulticlassMixin, torchmetrics.classification.MulticlassAccuracy):
+    pass
